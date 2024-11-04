487b25992f78ce3d7cf3a9093797ecbb3f1f0cb1
prdunlap <122948634+prdunlap@users.noreply.github.com>
2024-11-04 13:17:04 -0600

    Create index.php

---
+++ index.php
@@ -0,0 +1,10 @@
+<html>
+  <body>
+    <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/chart.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/lodash@4.17.21/lodash.min.js"></script>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/aos/2.3.4/aos.css">
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/aos/2.3.4/aos.js"></script>
+  </body>
+  
+</html>
